b02299d426218d22d209b496d27b673d2f5ae468
PHOENUX\Brandon <bdstevenson4225@aol.com>
2020-02-23 22:56:18 -0500

    initial push

---
+++ server/server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+
+
+var app = express();
+
+//Variable port  w/ default 8080
+var PORT = process.env.PORT || 8080;
+
+//Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Routes for html pages
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+//server page request listener
+app.listen(PORT, function () {
+	console.log("App listening on PORT: " + PORT);
+});  
