19fe2c000862c5804e77b5d3bfef8623adf50ac8
PHOENUX\Brandon <bdstevenson4225@aol.com>
2020-05-13 23:30:23 -0400

    config.json changed

--- backend/routes/apiRoutes.js
+++ backend/routes/apiRoutes.js
@@ -16,6 +16,7 @@ module.exports = function (app) {
   app.get("/api/all", (req, res) => {
     db.Stock.findAll({}).then(function (results) {
       res.json(results);
+      console.log('Server API: ')
     });
   })
 
@@ -23,25 +24,28 @@ module.exports = function (app) {
     db.Stock.findAll({ where: { category: req.params.category } }).then(function (results) {
       res.json(results);
     });
+    console.log('Server API: ')
   })
+
   app.get("/api/featured/:category", (req, res) => {
     db.Stock.findAll({ where: { category: req.params.category, featured: true }, limit: 10 }).then(function (results) {
       res.json(results);
-      console.log(req.params.product);
-    });
+      console.log('Server API: ')  
+      });
   })
 
   app.get("/api/:category/:subcategory", (req, res) => {
     db.Stock.findAll({ where: { subcategory: req.params.subcategory } }).then(function (results) {
       res.json(results);
+      console.log('Server API: ')    
     });
   })
 
   app.get("/api/:category/:subcategory/:product", (req, res) => {
     db.Stock.findAll({ where: { product_name: req.params.product } }).then(function (results) {
       res.json(results);
-      console.log(req.params.product);
-    });
+      console.log('Server API: ')   
+     });
   })
 
 
--- client/src/Utils/API.js
+++ client/src/Utils/API.js
@@ -5,22 +5,26 @@ export default {
     return axios.get("/api/all");
   },
   getSpecificProduct: function (cat, subcat, product) {
-    console.log(cat);
-    console.log(subcat);
-    console.log(product);
+    // console.log(cat);
+    // console.log(subcat);
+    // console.log(product);
+    console.log('Client API: ' + product)
     return axios.get("/api/" + cat + "/" + subcat + "/" + product);
   },
   getCategory: function (category) {
-    console.log(category);
+    // console.log(category);
+    console.log('Client API: ' + category)
     return axios.get("/api/" + category);
   },
   getSubCategory: function (cat, subcat) {
-    console.log(cat);
-    console.log(subcat);
+    // console.log(cat);
+    // console.log(subcat);
+    console.log('Client API: ' + subcat)
     return axios.get("/api/" + cat + "/" + subcat);
   },
   getFeaturedProducts: function (cat) {
-    console.log(cat)
+    // console.log(cat)
+    console.log('Client API: feature- ' + cat)
     return axios.get('/api/featured/' + cat)
   }
 }
