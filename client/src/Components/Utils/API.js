5c2275f278a2fa677a0fcd518aeca800bf1e433e
PHOENUX\Brandon <bdstevenson4225@aol.com>
2020-05-16 03:34:02 -0400

    api/ added back to client side api.js, JAWSDB_URL added as proxy to client package.json

--- client/src/Components/Utils/API.js
+++ client/src/Components/Utils/API.js
@@ -9,22 +9,22 @@ export default {
     // console.log(subcat);
     // console.log(product);
     console.log('Client API: ' + product)
-    return axios.get("/" + cat + "/" + subcat + "/" + product);
+    return axios.get("/api/" + cat + "/" + subcat + "/" + product);
   },
   getCategory: function (category) {
     // console.log(category);
     console.log('Client API: ' + category)
-    return axios.get("/" + category);
+    return axios.get("/api/" + category);
   },
   getSubCategory: function (cat, subcat) {
     // console.log(cat);
     // console.log(subcat);
     console.log('Client API: ' + subcat)
-    return axios.get("/" + cat + "/" + subcat);
+    return axios.get("/api/" + cat + "/" + subcat);
   },
   getFeaturedProducts: function (cat) {
     // console.log(cat)
     console.log('Client API: feature- ' + cat)
-    return axios.get('/featured/' + cat)
+    return axios.get('/api/featured/' + cat)
   }
 }
