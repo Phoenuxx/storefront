38c511c9343273ca963fed1f066f67dc0fd5aee9
PHOENUX\Brandon <bdstevenson4225@aol.com>
2020-05-18 22:54:05 -0400

    changed console logs on server side API routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -23,28 +23,28 @@ module.exports = function (app) {
   app.get("/api/:category/", (req, res) => {
     db.Stock.findAll({ where: { category: req.params.category } }).then(function (results) {
       res.json(results);
-      console.log('Server API: ' + results)
+      console.log('Server API: ' + req)
     });
   })
 
   app.get("/api/featured/:category", (req, res) => {
     db.Stock.findAll({ where: { category: req.params.category, featured: true }, limit: 10 }).then(function (results) {
       res.json(results);
-      console.log('Server API: ' + results)
+      console.log('Server API: ' + req)
     });
   })
 
   app.get("/api/:category/:subcategory", (req, res) => {
     db.Stock.findAll({ where: { subcategory: req.params.subcategory } }).then(function (results) {
       res.json(results);
-      console.log('Server API: ' + results)
+      console.log('Server API: ' + req)
     });
   })
 
   app.get("/api/:category/:subcategory/:product", (req, res) => {
     db.Stock.findAll({ where: { product_name: req.params.product } }).then(function (results) {
       res.json(results);
-      console.log('Server API: ' + results)
+      console.log('Server API: ' + req)
     });
   })
 
